ee81a1c7b4c8d885d6b26fa6cb17a271267e79e6
admin <mmd83@63.ru>
2022-12-15 19:45:18 +0400

    HomeWork12 На проверку Использована агрегация, спасибо за видео

--- src/pro/sky/java/course1/lesson12/Book.java
+++ src/pro/sky/java/course1/lesson12/Book.java
@@ -2,10 +2,10 @@ package pro.sky.java.course1.lesson12;
 
 public class Book {
     private String nameBook;
-    private String author;
+    private final Author author;
     private int year;
-
-    public Book(String nameBook, String author, int year) {
+    //в конструкторе агрегация, т.к. один автор может быть автором нескольких книг
+    public Book(String nameBook, Author author, int year) {
         this.nameBook = nameBook;
         this.author = author;
         this.year = year;
@@ -13,21 +13,21 @@ public class Book {
     public String getNameBook() {
         return nameBook;
     }
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
     public int getYear() {
         return year;
     }
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setNameBook() {
+        this.nameBook = nameBook;
     }
     public void setYear(int year) {
         this.year = year;
     }
     public static void display(Book book){
         System.out.println("\nНазвание: " + book.getNameBook());
-        System.out.println("Автор книги : " + book.getAuthor());
-        System.out.println("Год выпуска книги : " + book.getYear());
+        System.out.printf("Автор книги : %s %s", book.getAuthor().getName(), book.getAuthor().getFamily());
+        System.out.println("\nГод выпуска книги : " + book.getYear());
     }
 }
